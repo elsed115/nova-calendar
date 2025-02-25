27031bd1f427c9f4336a50db616dd54c1bec61ed
Giuliano Di Nardo <giulianodinardo@codeloops.it>
2025-02-25 12:54:04 +0100

    Update Tool.vue

--- resources/js/pages/Tool.vue
+++ resources/js/pages/Tool.vue
@@ -17,8 +17,9 @@
     <Head :title="$data.windowTitle || $data.title" />
 
     <div id="nc-control">
-   
-       <a @click="prevMonth" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + ←">
+    
+      <div class="left-items">
+        <a @click="prevMonth" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + ←">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" /></svg>
         </a>
 
@@ -29,7 +30,6 @@
         <a @click="nextMonth" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + →">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" /></svg>
         </a>
-        
         
         <Dropdown
           :handle-internal-clicks="true"
