3f4f1a3c0c6adf9ab46549e94b539786d7d17c37
Giuliano Di Nardo <giulianodinardo@codeloops.it>
2025-02-25 12:52:52 +0100

    Update Tool.vue

--- resources/js/pages/Tool.vue
+++ resources/js/pages/Tool.vue
@@ -18,7 +18,18 @@
 
     <div id="nc-control">
    
+       <a @click="prevMonth" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + ←">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" /></svg>
+        </a>
 
+        <a @click="reset" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + H">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><circle cx="12" cy="12" r="2" fill="currentColor"/></svg>
+        </a>
+          
+        <a @click="nextMonth" href="#" class="button hover:bg-gray-100 dark:hover:bg-gray-700" title="Alt + →">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M13 5l7 7-7 7M5 5l7 7-7 7" /></svg>
+        </a>
+        
         
         <Dropdown
           :handle-internal-clicks="true"
